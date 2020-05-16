34a5bc059223e8517d1d1562d2b7d318b8b286ee
Abel Fresnillo Silva <abel@MacBook-Pro.attlocal.net>
2020-05-16 14:09:32 -0400

    first commit

---
+++ src/main/java/dev/abel/jugattendeelist/JugAttendeeListApplication.java
@@ -0,0 +1,13 @@
+package dev.abel.jugattendeelist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JugAttendeeListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JugAttendeeListApplication.class, args);
+	}
+
+}
